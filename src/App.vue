5db368a5901c4485174f613c5186a4cac56758fe
凌海 <1275593712@qq.com>
2019-10-17 14:44:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<hello></hello>
+    <editor></editor>-->
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Editor from './components/Editor'
+import Home from './components/Echarts'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Editor,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import ECharts from 'vue-echarts'
+//按需加载引入
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+//注册组件
+Vue.component('chart', ECharts)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
